43e106a40efb4a787630327860cbd1222e6e6f82
mentallydisturbed <magzhan.zhumagazy@gmail.com>
2023-05-16 23:34:54 +0600

    mod4task4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if(first > second) {
+            if(third > first) System.out.println(third);
+            else System.out.println(first);
+        }else {
+            if(second > third) {
+                System.out.println(second);
+            }else System.out.println(third);
+        }
     }
 }
